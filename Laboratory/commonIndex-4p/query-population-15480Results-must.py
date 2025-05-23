7f89476ac78db8633fa2dc8d170eff80eb2f1b00
Juan Pablo Crossley <juan.crossley@mongodb.com>
2025-05-23 18:35:28 +0200

    Implemented must and equals to compare performance

---
+++ Laboratory/commonIndex-4p/query-population-15480Results-must.py
@@ -0,0 +1,214 @@
+import time
+from helper import execute_aggregation, JSONEncoder
+from datetime import datetime
+import json
+
+# Definir pipeline de agregación
+# Original
+pipeline = [
+   {
+       "$search": {
+           "index": "commonIndex-4p",
+           "returnStoredSource": True,
+           "compound": {
+               "filter": [
+                   {
+                       "embeddedDocument": {
+                           "path": "sn",
+                           "operator": {
+                               "compound": {
+                                   "must": [
+                                       {"equals": {"path": "sn.d.ani", "value": 240000}},
+                                       {"equals": {"path": "sn.a", "value": 18 }},
+                                       {"equals": {"path": "sn.a", "value": 10000 }},
+                                       {"equals": {"path": "sn.a", "value": 20000 }},
+                                       {"equals": {"path": "sn.a", "value": 30000 }},
+                                       {"equals": {"path": "sn.a", "value": 19 }},
+                                       {"in": {"path": "sn.d.v.df.cs", "value": [
+                                           "718-7", "62461000122102", "16676-9", "10851-4", "LL1937-3",
+                                           "88186-2", "77955-3", "41216-3", "7894-9", "16925-0", "38392-7",
+                                           "31870-9", "74408-6", "17327-8", "98080-5", "82301-3", "31946-7",
+                                           "31947-5", "21834-2", "21835-9", "31966-5", "21503-8", "60270-6",
+                                           "33689-1", "82731-1"
+                                       ]}}
+                                   ]
+                               }
+                           }
+                       }
+                   },
+                   {
+                       "embeddedDocument": {
+                           "path": "sn",
+                           "operator": {
+                               "compound": {
+                                   "must": [
+                                       {"equals": {"path": "sn.d.ani", "value": 10000}},
+                                       {"equals": {"path": "sn.a", "value": 18 }},
+                                       {"equals": {"path": "sn.a", "value": 10000 }},
+                                       {"equals": {"path": "sn.a", "value": 20000 }},
+                                       {"equals": {"path": "sn.a", "value": 30000 }},
+                                       {"equals": {"path": "sn.a", "value": 19 }},
+                                       {"equals": {"path": "sn.d.v.v", "value": "Negatiu"}}
+                                   ]
+                               }
+                           }
+                       }
+                   },
+                   {
+                       "embeddedDocument": {
+                           "path": "sn",
+                           "operator": {
+                               "compound": {
+                                   "filter": [
+                                       {"equals": {"path": "sn.d.T", "value": "C"}},
+                                       {"range": {
+                                           "path": "sn.d.cx.st.v",
+                                           "gte": datetime.fromisoformat("2024-06-19T00:00:00")
+                                       }}
+                                   ]
+                               }
+                           }
+                       }
+                   },
+                   {
+                       "embeddedDocument": {
+                           "path": "sn",
+                           "operator": {
+                               "compound": {
+                                   "must": [
+                                       {"equals": {"path": "sn.d.ani", "value": 10000}},
+                                       {"equals": {"path": "sn.a", "value": 10000}},
+                                       {"equals": {"path": "sn.a", "value": 3}},
+                                       {"range": {
+                                           "path": "sn.d.v.v",
+                                           "lte": datetime.fromisoformat("2021-01-01T08:04:47")
+                                       }}
+                                   ]
+                               }
+                           }
+                       }
+                   },
+                   {
+                       "embeddedDocument": {
+                           "path": "sn",
+                           "operator": {
+                               "compound": {
+                                   "must": [
+                                       {"equals": {"path": "sn.d.ani", "value": 110000}},
+                                       {"equals": {"path": "sn.a", "value": 10000}},
+                                       {"equals": {"path": "sn.a", "value": 3}},
+                                       {"equals": {"path": "sn.a", "value": 50000}},
+                                       {"equals": {"path": "sn.d.v.df.cs", "value": "H43001903"}}
+                                   ]
+                               }
+                           }
+                       }
+                   }
+               ]
+           }
+       }
+   },
+   { "$limit": 100 }
+]
+
+#pipeline = [
+#    {
+#        "$search": {
+#            "index": "commonIndex-4p",
+#            "returnStoredSource": True,
+#            "compound": {
+#                "filter": [
+#                    {
+#                        "embeddedDocument": {
+#                            "path": "sn",
+#                            "operator": {
+#                                "compound": {
+#                                    "filter": [
+#                                        {"equals": {"path": "sn.d.ani", "value": 240000}},
+#                                        {"equals": {"path": "sn.a", "value": 16 }},
+#                                        {"equals": {"path": "sn.a", "value": 17 }},
+#                                        {"equals": {"path": "sn.a", "value": 18 }},
+#                                        {"equals": {"path": "sn.a", "value": 10000 }},
+#                                        {"equals": {"path": "sn.a", "value": 20000 }},
+#                                        {"equals": {"path": "sn.a", "value": 30000 }},
+#                                        {"equals": {"path": "sn.a", "value": 19}},
+#                                        {"in": {"path": "sn.d.v.df.cs", "value": [
+#                                            "718-7", "62461000122102", "16676-9", "10851-4", "LL1937-3",
+#                                            "88186-2", "77955-3", "41216-3", "7894-9", "16925-0", "38392-7",
+#                                            "31870-9", "74408-6", "17327-8", "98080-5", "82301-3", "31946-7",
+#                                            "31947-5", "21834-2", "21835-9", "31966-5", "21503-8", "60270-6",
+#                                            "33689-1", "82731-1"
+#                                        ]}}
+#                                    ]
+#                                }
+#                            }
+#                        }
+#                    },
+#                    {
+#                        "embeddedDocument": {
+#                            "path": "sn",
+#                            "operator": {
+#                                "compound": {
+#                                    "filter": [
+#                                        {"equals": {"path": "sn.d.ani", "value": 10000}},
+#                                        {"in": {"path": "sn.a", "value": [16,17,18,10000,20000,30000,19]}},
+#                                        {"equals": {"path": "sn.d.v.v", "value": "Negatiu"}}
+#                                    ]
+#                                }
+#                            }
+#                        }
+#                    },
+#                    {
+#                        "embeddedDocument": {
+#                            "path": "sn",
+#                            "operator": {
+#                                "compound": {
+#                                    "filter": [
+#                                        {"equals": {"path": "sn.d.T", "value": "C"}},
+#                                        {"range": {
+#                                            "path": "sn.d.cx.st.v",
+#                                            "gte":  datetime.fromisoformat("2024-05-01")
+#                                        }}
+#                                    ]
+#                                }
+#                            }
+#                        }
+#                    },
+#                    {
+#                        "embeddedDocument": {
+#                            "path": "sn",
+#                            "operator": {
+#                                "compound": {
+#                                    "filter": [
+#                                        {"equals": {"path": "sn.d.ani", "value": 10000}},
+#                                        {"in": {"path": "sn.a", "value": [16,10000,3]}},
+#                                        {"range": {
+#                                            "path": "sn.d.v.v",
+#                                            "lte": datetime.fromisoformat("2024-01-01T08:04:47")
+#                                        }}
+#                                    ]
+#                                }
+#                            }
+#                        }
+#                    },
+#                    {
+#                        "embeddedDocument": {
+#                            "path": "sn",
+#                            "operator": {
+#                                "compound": {
+#                                    "filter": [
+#                                        {"equals": {"path": "sn.d.ani", "value": 110000}},
+#                                        {"in": {"path": "sn.a", "value": [16,10000,3,50000]}},
+#                                        {"equals": {"path": "sn.d.v.df.cs", "value": "H43001903"}}
+#                                    ]
+#                                }
+#                            }
+#                        }
+#                    }
+#                ]
+#            }
+#        }
+#    }
+#]
+results = execute_aggregation("CatSalutCDR.finalSearch", pipeline)
+
